ddbb734072b1a60f1ac127097e645cfb16f94c64
Sarah Proctor <sproctor950@gmail.com>
2024-03-21 16:04:57 -0700

    :sparkles: added header and navigation to each page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/home', to: 'home#index', as: 'index'
+  get '/compliments', to: 'home#compliments', as: 'compliments'
+  get '/contact', to: 'home#contact', as: 'contact'
+  get '/resume', to: 'home#resume', as: 'resume'
+  get '/story', to: 'home#story', as: 'story'
+  get '/work', to: 'home#work', as: 'work'
   root 'home#index'
 end
