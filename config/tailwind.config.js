cca2a008173e52d11e350ec39274ff5b6a7e80e7
Sarah Proctor <sproctor950@gmail.com>
2024-03-23 12:26:22 -0700

    :lipstick: adds header font and cleans up tailwind configs

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,28 +8,33 @@ module.exports = {
     "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+      md: "996px",
+      // => @media (min-width: 768px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
-        montserrat: ["montserrat", "sans-serif"],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        gistesy: ["gistesy", "sans-serif"],
       },
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
-      },
-      margin: {
-        sm: "8px",
-        md: "16px",
-        lg: "24px",
-        xl: "48px",
-      },
-    },
-    colors: {
-      blackFont: "#121f32",
-      pinkFont: "#ef8f7c",
     },
   },
-  plugins: [],
+  textColors: {
+    primary: "#121f32",
+    secondary: "#ef8f7c",
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
 }
