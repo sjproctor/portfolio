7d53fbf3c584efc6fe95d9c0f82725fc1330cb01
Sarah Proctor <sproctor950@gmail.com>
2024-03-23 12:29:56 -0700

    :fire: removes unnecessary page and all related navigation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get '/compliments', to: 'home#compliments', as: 'compliments'
   get '/contact', to: 'home#contact', as: 'contact'
   get '/resume', to: 'home#resume', as: 'resume'
-  get '/story', to: 'home#story', as: 'story'
   get '/work', to: 'home#work', as: 'work'
   root 'home#index'
 end
